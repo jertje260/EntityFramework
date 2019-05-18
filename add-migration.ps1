1a0e6875769dba72b932d13135342375180821fa
Jeroen Broekhuizen <jeroen.broekhuizen@jedlix.com>
2019-05-18 10:01:27 +0200

    updated powershell to have project & startup as vars

--- add-migration.ps1
+++ add-migration.ps1
@@ -1,6 +1,9 @@
+$PROJECT='".\Data\Data.csproj"'
+$STARTUP='".\Console\Console.csproj"'
+
 Write-Host "starting to create migrations"
 
-dotnet ef migrations add "$args" --project ".\Data\Data.csproj" --startup-project ".\Console\Console.csproj" --context FirstLibraryContext
-dotnet ef migrations add "$args" --project ".\Data\Data.csproj" --startup-project ".\Console\Console.csproj" --context SecondLibraryContext
+dotnet ef migrations add "$args" --project $PROJECT --startup-project $STARTUP --context FirstLibraryContext
+dotnet ef migrations add "$args" --project $PROJECT --startup-project $STARTUP --context SecondLibraryContext
 
 Write-Host "done creating all migrations"

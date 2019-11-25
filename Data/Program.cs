9954f4773810054e0d7d482c9f422e76ae3fdb9e
Jeroen Broekhuizen <jeroen.broekhuizen@jedlix.com>
2019-11-25 13:20:26 +0100

    added required packages, use the correct version of dotnet sdk

---
+++ Data/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("ConnectionString") ?? "";
+
+            var builder = new DbContextOptionsBuilder<DbContext>();
+
+            builder
+                .UseSqlServer(
+                    connectionString,
+                    opts => opts.CommandTimeout((int)TimeSpan.FromMinutes(120).TotalSeconds));
+
+            var context = new Context(builder.Options);
+
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
+        }
+    }
+}
